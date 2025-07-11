9de76d4afd259f55f50a3808f49e4310a2945f5c
LasiduDilshan <dilshanlasindu0@gmail.com>
2025-07-11 22:59:42 +0530

    Initiated codes

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>Email: your@email.com</p>
+      <p>
+        <a href="https://github.com/LasiduDilshan">GitHub</a>
+      </p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          <strong>Project 1:</strong> Description here.
+        </li>
+        <li>
+          <strong>Project 2:</strong> Description here.
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;
